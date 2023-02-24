c4cab540a742e67b246f9ec5de4154f1ba78dd6d
madarren <108047914+madarren@users.noreply.github.com>
2023-02-24 20:56:16 +1100

    Updated the website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,15 +14,15 @@ const kaisei = localFont({
 
 export const metadata: Metadata = {
   title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
+    default: 'Darren Ma',
+    template: '%s | Darren Ma',
   },
   description: 'Developer, writer, and creator.',
   openGraph: {
-    title: 'Lee Robinson',
+    title: 'Darren Ma',
     description: 'Developer, writer, and creator.',
     url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
+    siteName: 'Darren Ma',
     images: [
       {
         url: 'https://leerob.io/og.jpg',
